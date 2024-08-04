7c6ee553072a2125745bc630777019e4f7302e32
Beto Hernandez <guicholh@gmail.com>
2024-08-04 13:09:31 -0500

    updated theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -43,12 +43,29 @@
 --   },
 -- }
 --
+-- return {
+--   "kjssad/quantum.vim",
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "quantum",
+--     },
+--   },
+-- }
+
 return {
-  "kjssad/quantum.vim",
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "quantum",
-    },
-  },
+  "0xstepit/flow.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    require("flow").setup({
+      transparent = true, -- Set transparent background.
+      fluo_color = "orange", --  Fluo color: pink, yellow, orange, or green.
+      mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
+      aggressive_spell = false, -- Display colors for spell check.
+    })
+
+    vim.cmd("colorscheme flow")
+  end,
 }
