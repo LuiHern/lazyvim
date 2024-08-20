54f5bcd5a17bb41d1a03241d675da24fe158c0cb
Luis H. Hernandez <guicholh@gmail.com>
2024-08-19 22:38:56 -0500

    update

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -61,7 +61,7 @@ return {
   config = function()
     require("flow").setup({
       transparent = true, -- Set transparent background.
-      fluo_color = "orange", --  Fluo color: pink, yellow, orange, or green.
+      fluo_color = "pink", --  Fluo color: pink, yellow, orange, or green.
       mode = "normal", -- Intensity of the palette: normal, bright, desaturate, or dark. Notice that dark is ugly!
       aggressive_spell = false, -- Display colors for spell check.
     })
